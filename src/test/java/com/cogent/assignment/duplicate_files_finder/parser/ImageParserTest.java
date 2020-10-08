88becc7b77a7977567cf310fe661ae2f9b4b801a
Lovika Saxena <slovika@thoughtworks.com>
2020-10-09 00:56:46 +0530

    [Lovika] | Adds test for ImageParser for testing same sha for same images

--- src/test/java/com/cogent/assignment/duplicate_files_finder/parser/ImageParserTest.java
+++ src/test/java/com/cogent/assignment/duplicate_files_finder/parser/ImageParserTest.java
@@ -8,26 +8,60 @@ import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by lovikasaxena
  */
 class ImageParserTest {
 
-    private SHAGenerator shaGenerator = mock(SHAGenerator.class);
-    private ImageParser imageParser = new ImageParser(shaGenerator);
+    @Test
+    void should_parse_all_pixels_of_given_image_and_create_hash() throws IOException, NoSuchAlgorithmException {
+        String imagePath = this.getClass().getResource("/homeDir/tue.jpg").getPath();
+
+        SHAGenerator shaGenerator = mock(SHAGenerator.class);
+        ImageParser imageParserWithMock = new ImageParser(shaGenerator);
+        when(shaGenerator.createSHAFor(anyString())).thenReturn("random-sha");
+
+        String imageSHA = imageParserWithMock.createSHAForImage(imagePath);
+
+        assertEquals("random-sha", imageSHA);
+    }
 
 
     @Test
-    void should_parse_all_pixels_of_image_and_create_hash() throws IOException, NoSuchAlgorithmException {
-        String imagePath = this.getClass().getResource("/homeDir/tue.jpg").getPath();
+    void should_return_same_hash_when_both_images_are_same() throws IOException, NoSuchAlgorithmException {
+        String path1 = this.getClass().getResource("/homeDir/tue.jpg").getPath();
+        String path2 = this.getClass().getResource("/homeDir/tue_bnW.jpg").getPath();
 
-        when(shaGenerator.createSHAFor(anyString())).thenReturn("random-sha");
+        ImageParser imageParser = new ImageParser(new SHAGenerator());
+        String imageSHA1 = imageParser.createSHAForImage(path1);
+        String imageSHA2 = imageParser.createSHAForImage(path2);
 
-        String imageSHA = imageParser.createSHAForImage(imagePath);
+        assertFalse(imageSHA1.equals(imageSHA2));
+    }
 
-        assertEquals("random-sha", imageSHA);
+    @Test
+    void should_return_different_hash_when_images_are_same_but_have_color_difference() throws IOException, NoSuchAlgorithmException {
+        String path1 = this.getClass().getResource("/homeDir/tue.jpg").getPath();
+        String path2 = this.getClass().getResource("/homeDir/tue_bnW.jpg").getPath();
+
+        ImageParser imageParser = new ImageParser(new SHAGenerator());
+        String imageSHA1 = imageParser.createSHAForImage(path1);
+        String imageSHA2 = imageParser.createSHAForImage(path2);
+
+        assertFalse(imageSHA1.equals(imageSHA2));
+    }
+
+    @Test
+    void should_return_different_hash_when_images_are_different() throws IOException, NoSuchAlgorithmException {
+        String path1 = this.getClass().getResource("/homeDir/tue.jpg").getPath();
+        String path2 = this.getClass().getResource("/homeDir/barry.JPG").getPath();
+
+        ImageParser imageParser = new ImageParser(new SHAGenerator());
+        String imageSHA1 = imageParser.createSHAForImage(path1);
+        String imageSHA2 = imageParser.createSHAForImage(path2);
+
+        assertFalse(imageSHA1.equals(imageSHA2));
     }
 }
